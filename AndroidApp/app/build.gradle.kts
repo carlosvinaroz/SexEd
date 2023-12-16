2633ae8ee8bfe47d18aebddf3084b2b98d2d0737
Carlos R <carlos.work.upc@gmail.com>
2023-12-16 12:35:27 +0100

    [SEXED-001][FIX] Fix emulator
    Update compilation SDK version

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.sexed"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.sexed"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
